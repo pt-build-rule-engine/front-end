38a62ae2c02428e81bae4fea57563faa4abfb4cb
Joey Nevarez <josephnevarez12@gmail.com>
2020-01-30 18:51:53 -0800

    added signup and login actions and reducers, but will likely need to regfractor depending on backend

--- rule-engine/src/Actions/Login.js
+++ rule-engine/src/Actions/Login.js
@@ -4,7 +4,15 @@ export const LOGIN_START = "LOGIN_START";
 export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
 export const LOGIN_ERROR = "LOGIN_ERROR";
 
-export function login(credentials) {
-
-    
-}
+export const login = (credentials) => dispatch => {
+    dispatch({ type: LOGIN_START });
+    api().post("/auth/login", credentials)
+        .then(res => {
+            dispatch({ type: LOGIN_SUCCESS, payload: res.data })
+            localStorage.setItem("token", res.data.token);
+            localStorage.setItem("user_id", res.data.user.id);
+        })
+        .catch(err => {
+            dispatch({ type: LOGIN_ERROR, payload: err })
+        })
+};
--- rule-engine/src/App.js
+++ rule-engine/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 
 import './App.css';
 
@@ -15,4 +16,4 @@ function App() {
   );
 }
 
-export default App;
+export default withRouter(App);
