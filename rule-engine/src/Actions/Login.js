8e0762ade4860d90789ce924bf2e9b60239fc49f
Joey Nevarez <josephnevarez12@gmail.com>
2020-01-30 18:16:28 -0800

    added privateroute and log out components, started on an action file

---
+++ rule-engine/src/Actions/Login.js
@@ -0,0 +1,10 @@
+import api from '../Utls/api';
+
+export const LOGIN_START = "LOGIN_START";
+export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
+export const LOGIN_ERROR = "LOGIN_ERROR";
+
+export function login(credentials) {
+
+    
+}
--- rule-engine/src/App.js
+++ rule-engine/src/App.js
@@ -9,7 +9,8 @@ function App() {
 
     {/**Routes below */}
 
-    
+
+
     </div>
   );
 }
