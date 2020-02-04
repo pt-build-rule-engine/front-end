45b6db3f82f89570216024be9feb357c5e4412ab
Trevor Thompson <trevthom18@gmail.com>
2020-02-03 20:38:40 -0500

    Committing changes with errors so they aren't lost

--- rule-engine/src/Components/SignUp.js
+++ rule-engine/src/Components/SignUp.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { withFormik, Form, Field } from "formik";
+import { withFormik, Form, Field, yupToFormErrors } from "formik";
 import * as Yup from "yup";
 
 
 const SignUp = (props) => {
 
-    const handleSubmit = props => {
+    const handleSubmit = event => {
 
         event.preventDefault();
     };
@@ -43,6 +43,33 @@ const SignUp = (props) => {
            </Form>     
         </div>
     );
-};
+}
+
+const enhancedForm = withFormik ({
+    mapPropsToValues({companyName, email, phoneNumber} => ({
+
+        companyName: companyName ||" ",
+        email: email || " ",
+        phoneNumber: phoneNumber || " "
+
+    }),
+
+    validationSchema: Yup.object().shape({
+
+        companyName: Yup.string()
+        .max(20, 'CompanyName must be shorter than 20 characters')
+        .required('Username is required!'),
+
+        email: Yup.string()
+        .min(5, 'Invalid email')
+        .max(30, 'Email must be shorter than 30 characters')
+        .required('Email is required'),
+
+        phoneNumber: Yup.string()
+        .min(10, 'Phone number needs to at least be 10 characters')
+        .required('Phone Number is required')
+
+    })
+})(MyInnerForm);
 
 export default SignUp;
