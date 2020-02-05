531ca40acd104e64faedf9e7263ad06bbd82a6bc
Joey Nevarez <josephnevarez12@gmail.com>
2020-02-04 21:44:35 -0800

    restructured contactList, added getIndContact files

---
+++ rule-engine/src/Components/SearchForm.js
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from 'react';
+import { withFormik, Form, Field } from "formik";
+
+
+const SearchForm = (status) => {
+
+    const StyledDiv = styled.div`
+    display: flex;
+    justify-content: center;
+    padding-top: 20px;
+    `;
+
+    const [searchTerm, setSearchTerm] = useState("");
+
+    useEffect(() => {
+        setSearchTerm(status);
+    }, [status]);
+
+    let errorStyle = {
+        color: "red"
+    };
+
+    return (
+        <div>
+        <StyledDiv>
+            <Form>
+                <Field type="text" name="search" placeholder="Search..." />
+            </Form>
+        </StyledDiv>
+        <StyledDiv>
+            <CharacterList searchArg={searchTerm} />
+        </StyledDiv>
+        </div>
+    );
+}
+
+const FormikSearchForm = withFormik({
+    mapPropsToValues({ search }) {
+        return {
+            search: search || "",
+        };
+    },
+
+    handleSubmit(values, { setStatus }) {
+        setStatus(values);
+    }
+
+})(SearchForm);
+
+export default SearchForm;
